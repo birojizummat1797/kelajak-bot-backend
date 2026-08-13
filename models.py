4ce29808a9bd0f3f6d99b250d9a1c6f9a98b9472
birojizummat1797 <birojizummat@gmail.com>
2026-08-13 17:53:45 +0500

    Baza yangi V2 toza jadvallarga otkazildi

--- models.py
+++ models.py
@@ -1,18 +1,17 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, JSON
-from sqlalchemy.sql import func
+from sqlalchemy import Column, Integer, String, JSON, ForeignKey
 from database import Base
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "customers_v2"  # ESKI users O'RNIGA TOZA JADVAL
+
     id = Column(Integer, primary_key=True, index=True)
     telegram_id = Column(String, unique=True, index=True)
-    full_name = Column(String, nullable=True) 
-    phone_number = Column(String, nullable=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    full_name = Column(String)
+    phone_number = Column(String)
 
 class SurveyResponse(Base):
-    __tablename__ = "survey_responses"
+    __tablename__ = "survey_v2"  # ESKI survey_responses O'RNIGA
+
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
-    raw_answers = Column(JSON, nullable=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    user_id = Column(Integer, ForeignKey("customers_v2.id"))
+    raw_answers = Column(JSON)  # Ma'lumotlar to'g'ridan-to'g'ri JSON saqlanadi
